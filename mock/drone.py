7e6666dfd48b5c94b8ff10a6c4fc7d64c83126ab
rafael <rafael.avigad@nmtafe.wa.edu.au>
2023-05-12 15:52:39 +0800

    improved the interface

--- mock/drone.py
+++ mock/drone.py
@@ -1,27 +1,72 @@
 import socket
-import sys
 import time
 
-HOST, PORT = "127.0.0.1", 8999
-STATE_UDP_PORT = 8890
-data = " ".join(sys.argv[1:])
+
+class TelloDrone:
+    def __init__(self, host="10.25.35.130", port=8889, state_udp_port=8890):
+        self.host = host
+        self.port = port
+        self.state_udp_port = state_udp_port
+        self.command_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.command_socket.bind(("", port))
+        self.state_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.state_socket.bind(("", self.state_udp_port))
+        self.send_command(
+            "command"
+        )  # This is the command to start the SDK (command) mode of the drone
+
+    def send_command(self, command):
+        print(f"Sending command: {command}")
+        self.command_socket.sendto(bytes(command, "utf-8"), (self.host, self.port))
+        received = str(self.command_socket.recv(1024), "utf-8")
+        print(f"Received: {received}")
+        time.sleep(1)  # Giving some time for command to take effect
+
+    def set_ap_mode(self, ssid, password):
+        print(f"SENDING: |ap {ssid} {password}|")
+        self.send_command(f"ap {ssid} {password}")
+
+    def takeoff(self):
+        self.send_command("takeoff")
+
+    def land(self):
+        self.send_command("land")
+
+    def process_telemetry(self):
+        for _ in range(5):
+            data, address = self.state_socket.recvfrom(1024)
+            address = address[0]
+            print(f"\nData received from {address} at state_socket")
+            telemetry_data = data.decode("ASCII").split(";")
+            telemetry_dict = dict(
+                telem.split(":") for telem in telemetry_data if telem and ":" in telem
+            )
+            for key, value in telemetry_dict.items():
+                print(f"{key}: {value}")
+            time.sleep(1)
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((HOST, 0))
+if __name__ == "__main__":
+    drone = TelloDrone()
 
-received = str(sock.recv(1024), "utf-8")
-sock.sendto(bytes(data, "utf-8"), (HOST, PORT))
+    print(
+        "Commands: ap_mode <ssid> <password>, takeoff, land, telemetry, <free form command>, quit"
+    )
 
-print(f"Sent:     {data}")
-print(f"Received: {received}")
-time.sleep(4)
-state_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-state_socket.bind(("", STATE_UDP_PORT))
+    while True:
+        command = input("Enter command: ")
 
-while True:
-    data, address = state_socket.recvfrom(1024)
-    address = address[0]
-    print(f'Data received from {address} at state_socket')
-
-    print("Data:", data.decode('ASCII'))
+        if command == "quit":
+            break
+        elif command.startswith("ap_mode "):
+            _, ssid, password = command.split()
+            drone.set_ap_mode(ssid, password)
+        elif command == "takeoff":
+            drone.takeoff()
+        elif command == "land":
+            drone.land()
+        elif command == "telemetry":
+            print("Telemetry started...")
+            drone.process_telemetry()
+        else:
+            drone.send_command(command)
