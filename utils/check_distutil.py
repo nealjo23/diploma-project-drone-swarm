4b178be8d12152a119411b109d32b86cd240dcb1
Rafael Avigad <avigar@tdm.local>
2023-05-19 09:08:04 +0800

    added utils

---
+++ utils/check_distutil.py
@@ -0,0 +1,28 @@
+import sys
+import platform
+import struct
+
+print("Python version:")
+print(sys.version)
+
+
+print("\nPython executable:")
+print(sys.executable)
+
+
+
+print("\nIs Python 64-bit?")
+print(struct.calcsize('P') * 8 == 64)
+
+print("Endianess")
+print(sys.byteorder)
+
+print("\nPlatform:")
+print(platform.platform())
+
+
+
+print("\nPython path:")
+
+for path in sys.path:
+    print(path)
