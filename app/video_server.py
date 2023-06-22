a4eb51775baaa05a3963b1590e4172c758ad9032
John Neal <johnwneal@yahoo.com>
2023-06-22 23:23:59 +0800

    Changed video server to streaming

--- app/video_server.py
+++ app/video_server.py
@@ -1,13 +1,20 @@
-from flask import Flask, send_file
+from flask import Flask, Response, stream_with_context
 
 app = Flask(__name__)
 
 
 @app.route('/video')
 def stream_video():
-    video_path = 'stock_monitoring.mp4'
+    video_path = 'drone_herd.mp4'
 
-    return send_file(video_path, as_attachment=False, mimetype='video/mp4')
+    def generate():
+        with open(video_path, 'rb') as video_file:
+            while True:
+                chunk = video_file.read(1024 * 1024)  # reading in 1MB chunks
+                if not chunk:
+                    break
+                yield chunk
+    return Response(stream_with_context(generate()), content_type='video/mp4')
 
 
 if __name__ == '__main__':
